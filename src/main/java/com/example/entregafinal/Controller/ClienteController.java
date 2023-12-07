4999f359828e519d24c790bcf399a6910446f99f
Francisco Salvetti <franciscosalvetti4@gmail.com>
2023-12-07 19:43:01 -0300

    modificación importantisima

--- src/main/java/com/example/entregafinal/Controller/ClienteController.java
+++ src/main/java/com/example/entregafinal/Controller/ClienteController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("api/v1/clientes")
 @RestController
-public class ClienteController {
+public class ClienteController{
 
     @Autowired
     ClienteService clienteService;
